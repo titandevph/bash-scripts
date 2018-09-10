5ac9bd502e59dc8941a31f49a3449ee86d5c884b
titandevph <titan@titandev.org>
2018-09-10 19:02:04 +0800

    Create nginx-php-install.sh

---
+++ nginx-php-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get remove -y php* nginx apache* git
+sudo apt-get purge -y php* nginx apache* git
+
+sudo apt-get install -y git nginx php5-fpm unzip curl
+sudo mkdir /var/www/
+sudo mkdir /var/www/html
+sudo service nginx stop
+sudo rm -rf /etc/nginx/nginx.conf
+cd /etc/nginx
+wget -c https://raw.githubusercontent.com/titandevph/bash-scripts/master/nginx.conf
+sudo service php5-fpm restart
+sudo service nginx restart
